cf0c162df2566d02ede068f5c201fb3ca06d183f
Hasibul <hasibulhasanjoy.007@gmail.com>
2025-07-22 08:56:04 +0600

    feat: implement database connection and update server configuration

---
+++ src/config/db.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const connectionString = process.env.MONGO_URI;
+    await mongoose.connect(connectionString!);
+    console.log(`Database connected successfully`);
+  } catch (error) {
+    console.log(`Database connection failed`);
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import app from "./app.js";
+import connectDB from "./config/db.js";
 
-const PORT: number = 5500;
+const PORT: number = Number(process.env.PORT) || 5500;
+
+await connectDB();
 
 app.listen(PORT, () => {
   console.log(`server is running on port:${PORT}`);
