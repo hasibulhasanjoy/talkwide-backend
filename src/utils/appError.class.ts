4c9e708497f24aacbff7873806dd780e4124b1af
Hasibul <hasibulhasanjoy.007@gmail.com>
2025-07-24 21:04:37 +0600

    chore: remove unnecessary blank line in AppError class

--- src/utils/appError.class.ts
+++ src/utils/appError.class.ts
@@ -2,7 +2,7 @@ class AppError extends Error {
   public readonly statusCode: number;
   public readonly status: string;
   public readonly isOperational: boolean;
-  
+
   constructor(message: string, statusCode: number) {
     super(message);
 
