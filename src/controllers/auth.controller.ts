79ed2e4d8dbda4fed0bebb4ac837e9c3a07a12af
Hasibul <hasibulhasanjoy.007@gmail.com>
2025-08-04 23:39:27 +0600

    feat: implement login functionality with password validation and token generation

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,9 +2,11 @@ import { NextFunction, Request, Response } from "express";
 
 import IUser from "../interfaces/user.interface.js";
 import User from "../models/user.model.js";
-import { SignUpData } from "../schemas/auth.schema.js";
+import { LoginData, SignUpData } from "../schemas/auth.schema.js";
 import AppError from "../utils/appError.class.js";
 import asyncErrorHandler from "../utils/asyncErrorHandler.utils.js";
+import { promises } from "dns";
+import bcrypt from "bcrypt";
 
 export const signUp = asyncErrorHandler(
   async (req: Request, res: Response, _next: NextFunction): Promise<void | Response> => {
@@ -34,6 +36,35 @@ export const signUp = asyncErrorHandler(
           displayName,
           email,
         },
+      },
+    });
+  }
+);
+
+export const login = asyncErrorHandler(
+  async (req: Request, res: Response, _next: NextFunction): Promise<void | Response> => {
+    const { username, email, password } = req.body as LoginData;
+    const existingUser = await User.findOne({ $or: [{ username }, { email }] }).select("+password");
+
+    if (!existingUser) {
+      throw new AppError("invalid credential", 401);
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
+
+    if (!isPasswordCorrect) {
+      throw new AppError("invalid credential", 401);
+    }
+
+    const token = existingUser.generateAuthToken();
+
+    res.status(200).json({
+      status: "success",
+      token,
+      data: {
+        username: existingUser.username,
+        displayName: existingUser.displayName,
+        email: existingUser.email,
       },
     });
   }
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,11 +1,12 @@
 import express, { Router } from "express";
 
-import { signUp } from "../controllers/auth.controller.js";
+import { login, signUp } from "../controllers/auth.controller.js";
 import { validate } from "../middlewares/validate.middleware.js";
-import { signUpSchema } from "../schemas/auth.schema.js";
+import { loginSchema, signUpSchema } from "../schemas/auth.schema.js";
 
 const router: Router = express.Router();
 
 router.route("/signup").post(validate(signUpSchema), signUp);
+router.route("/login").post(validate(loginSchema), login);
 
 export default router;
