9c1e35e9995f08ba3dee50bc9120614dc342dba7
Hasibul <hasibulhasanjoy.007@gmail.com>
2025-08-24 23:11:19 +0600

    fix: remove unnecessary blank line in change password function

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -145,7 +145,7 @@ export const changePassword = asyncErrorHandler(
     const { oldPassword, newPassword } = req.body as ChangePasswordData;
 
     const isPasswordCorrect = await user.comparePassword(oldPassword);
-    
+
     if (!isPasswordCorrect) {
       return next(new AppError("incorrect password", 401));
     }
